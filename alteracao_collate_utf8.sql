834ee9ba2c906f118a8fc5c00533de855cbe91b2
Osmar Filho <osmarfilho6@gmail.com>
2017-01-31 14:30:59 -0300

    Atualização do CHARSET E collate das tabelas e do banco

---
+++ alteracao_collate_utf8.sql
@@ -0,0 +1,12 @@
+ALTER TABLE fornecedores CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE contatos CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE usuario CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE pgtos CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE pgtos_parcela CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE pgtos_quitacao CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE setores CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE contas CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+alter DATABASE `webfinancas` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+
